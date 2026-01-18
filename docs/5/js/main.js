a418076008f669f85f2a32ca19a11ca9421aadf6
ytyaru <yttry0@gmail.com>
2026-01-18 15:02:45 +0900

    add

---
+++ docs/5/js/main.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOMContentLoaded!!');
+    const author = 'ytyaru';
+    van.add(document.querySelector('main'), 
+        van.tags.h1(van.tags.a({href:`https://github.com/${author}/JS.Ruby2.20260117165252/`}, 'Ruby2')),
+        van.tags.p('二重ルビ。'),
+//        van.tags.p('Double ruby.'),
+    );
+    van.add(document.querySelector('footer'),  new Footer('ytyaru', '../').make());
+
+    const a = new Assertion();
+    a.t(true);
+    a.f(false);
+    a.e(TypeError, `msg`, ()=>{throw new TypeError(`msg`)});
+    a.fin();
+});
+window.addEventListener('beforeunload', (event) => {
+    console.log('beforeunload!!');
+});
+
